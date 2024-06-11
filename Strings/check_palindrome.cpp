d852e69460a076b6241aa706bc5831040e1ec1a3
jadonsooraj <jadonsuraj49@gmail.com>
2024-06-11 12:49:20 +0530

    problem add

---
+++ Strings/check_palindrome.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(string s){
+    for(char c:s) cout<<c;
+
+    cout<<endl;
+}
+
+bool checkPalindrome(string s){
+    int front=0, back=s.size()-1;
+
+    while(front<=back){
+        if(s[front]==s[back]){
+            front++;
+            back--;
+        }
+        else return false;
+    }
+    return true;
+    
+}
+main(){
+    string s;
+    int size;
+
+    cout<<"Enter size of string: ";
+    cin>>size;
+
+    cout<<"\n Enter element: "<<endl;
+    for(int i=0;i<size;i++){
+        char ele;
+        cin>>ele;
+        s.push_back(ele);
+    }
+    display(s);
+
+    if(checkPalindrome(s)) cout<<"true";
+    else cout<<"False";
+    return 0;
+}
